3c25c8f558f607fe9c5641b13ef3577e552ab4ea
Exowz <53607938+Exowz@users.noreply.github.com>
2025-02-11 12:00:00 +0100

    Produit

---
+++ Produit/Main.java
@@ -0,0 +1,11 @@
+package Produit;
+
+public class Main {
+    public static void main(String[] args) {
+        Produit livre = new Livre("Les Miserables", 20.0);
+        Produit dvd = new DVD("Inception", 15.0);
+        
+        System.out.println("Prix final du livre: " + livre.calculerPrix() + " EUR");
+        System.out.println("Prix final du DVD: " + dvd.calculerPrix() + " EUR");
+    }
+}
---
+++ Produit/Produit.java
@@ -0,0 +1,42 @@
+package Produit;
+
+public class Produit {
+    protected String nom;
+    protected double prixHT;
+
+    public Produit(String nom, double prixHT) {
+        this.nom = nom;
+        this.prixHT = prixHT;
+    }
+
+    public double calculerPrix() {
+        return prixHT;
+    }
+}
+
+class Livre extends Produit {
+    private static final double TVA_LIVRE = 0.05;
+
+    public Livre(String nom, double prixHT) {
+        super(nom, prixHT);
+    }
+
+    @Override
+    public double calculerPrix() {
+        return prixHT * (1 + TVA_LIVRE);
+    }
+}
+
+class DVD extends Produit {
+    private static final double TVA_DVD = 0.20;
+
+    public DVD(String nom, double prixHT) {
+        super(nom, prixHT);
+    }
+
+    @Override
+    public double calculerPrix() {
+        return prixHT * (1 + TVA_DVD);
+    }
+}
+
